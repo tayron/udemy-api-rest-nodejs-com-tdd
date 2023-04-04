1d6295d9e7d7f87598e94a7557f9d37b1bd52803
tayron <ti@hospeda.app>
2023-04-04 16:37:04 -0300

    Implementado reuso de estrutura em teste unitário de transactions

--- test/routes/transaction.test.js
+++ test/routes/transaction.test.js
@@ -16,7 +16,7 @@ let user2
 let accountUser1
 let accountUser2
 
-describe.only('Transactions', () => {
+describe('Transactions', () => {
   beforeAll(async () => {
     await app.db(TABLE_TRANSACTIONS).del()
     await app.db(TABLE_ACCOUNTS).del()
@@ -117,111 +117,47 @@ describe.only('Transactions', () => {
       })
   })
 
-  test('Não deve inserir uma transação sem descrição', async () => {
-    const transaction = {
-      date: new Date(),
-      ammount: 100,
-      type: 'SAIDA',
-      account_id: accountUser1.id
-    }
-
-    return await request(app).post(MAIN_ROUTE)
-      .set('authorization', `bearer ${user1.token}`)
-      .send(transaction)
-      .then(res => {
-        expect(res.status).toBe(400);
-        expect(res.body.error).toBe('Descrição da transação é obrigatório');
-      })
-  })
-
-  test('Não deve inserir uma transação sem valor', async () => {
-    const transaction = {
-      description: 'new T SAIDA',
-      date: new Date(),
-      //ammount: 100,
-      type: 'SAIDA',
-      account_id: accountUser1.id
-    }
-
-    return await request(app).post(MAIN_ROUTE)
-      .set('authorization', `bearer ${user1.token}`)
-      .send(transaction)
-      .then(res => {
-        expect(res.status).toBe(400);
-        expect(res.body.error).toBe('Valor da transação é obrigatório');
-      })
-  })
-
-  test('Não deve inserir uma transação sem data', async () => {
-    const transaction = {
-      description: 'new T SAIDA',
-      ammount: 100,
-      type: 'SAIDA',
-      account_id: accountUser1.id
-    }
-
-    return await request(app).post(MAIN_ROUTE)
-      .set('authorization', `bearer ${user1.token}`)
-      .send(transaction)
-      .then(res => {
-        expect(res.status).toBe(400);
-        expect(res.body.error).toBe('Data da transação é obrigatório');
-      })
-  })
-
-  test('Não deve inserir uma transação sem conta', async () => {
+  const templateTestTransacaoIbvalida = async (newData, errorMessage) => {
     const transaction = {
       description: 'new T SAIDA',
       date: new Date(),
       ammount: 100,
       type: 'SAIDA',
-      //account_id: accountUser1.id
-    }
-
-    return await request(app).post(MAIN_ROUTE)
-      .set('authorization', `bearer ${user1.token}`)
-      .send(transaction)
-      .then(res => {
-        expect(res.status).toBe(400);
-        expect(res.body.error).toBe('Conta referente à transação é obrigatória');
-      })
-  })
-
-  test('Não deve inserir uma transação sem tipo', async () => {
-    const transaction = {
-      description: 'new T SAIDA',
-      date: new Date(),
-      ammount: 100,
-      // type: 'SAIDA',
       account_id: accountUser1.id
     }
 
     return await request(app).post(MAIN_ROUTE)
       .set('authorization', `bearer ${user1.token}`)
-      .send(transaction)
+      .send({ ...transaction, ...newData })
       .then(res => {
         expect(res.status).toBe(400);
-        expect(res.body.error).toBe('Tipo da transação é obrigatório');
+        expect(res.body.error).toBe(errorMessage);
       })
-  })
+  }
 
-  test('Não deve inserir uma transação sem tipo inválido', async () => {
-    const transaction = {
-      description: 'new T SAIDA',
-      date: new Date(),
-      ammount: 100,
-      type: 'RETIRADA',
-      account_id: accountUser1.id
-    }
+  test('Não deve inserir uma transação sem descrição',
+    async () => templateTestTransacaoIbvalida(
+      { description: null }, 'Descrição da transação é obrigatório'))
 
-    return await request(app).post(MAIN_ROUTE)
-      .set('authorization', `bearer ${user1.token}`)
-      .send(transaction)
-      .then(res => {
-        expect(res.status).toBe(400);
-        expect(res.body.error).toBe('Tipo da transação inválida');
-      })
-  })
+  test('Não deve inserir uma transação sem valor',
+    async () => templateTestTransacaoIbvalida(
+      { ammount: null }, 'Valor da transação é obrigatório'))
+
+  test('Não deve inserir uma transação sem data',
+    async () => templateTestTransacaoIbvalida(
+      { date: null }, 'Data da transação é obrigatório'))
+
+  test('Não deve inserir uma transação sem conta',
+    async () => templateTestTransacaoIbvalida(
+      { account_id: null }, 'Conta referente à transação é obrigatória'))
+
+  test('Não deve inserir uma transação sem tipo',
+    async () => templateTestTransacaoIbvalida(
+      { type: null }, 'Tipo da transação é obrigatório'))
+
+  test('Não deve inserir uma transação com tipo errado',
+    async () => templateTestTransacaoIbvalida(
+      { type: 'RETIRADA' }, 'Tipo da transação inválida'))
 
   test('Deve retornar uma transação por id', async () => {
     const transaction = {
