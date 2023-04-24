fa351a5a7f7c3bb730af98d7bcc0dcc85f56420e
tayron <ti@hospeda.app>
2023-04-24 12:25:44 -0300

    Testando husk

--- src/services/balance.js
+++ src/services/balance.js
@@ -14,7 +14,9 @@ module.exports = (app) => {
       .groupBy(`${ACCOUNTS_TABLE}.id`)
       .orderBy(`${ACCOUNTS_TABLE}.id`)
 
-    return balance ? JSON.parse(JSON.stringify(balance)) : null
+    return balance
+
+      ? JSON.parse(JSON.stringify(balance)) : null
   }
 
   return { getBalanceByUserId }
